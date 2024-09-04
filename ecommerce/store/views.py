742b840c43ef6316790302e40b34ddcd5ea20d92
Erdinc Mutlu <erdinc.mutlu@fiveaxis.health>
2024-09-04 17:27:47 +0100

    Create singular product page

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from .models import Category, Product
-
+from django.shortcuts import get_object_or_404
 
 def store(request):
     all_products = Product.objects.all()
@@ -14,3 +14,9 @@ def categories(request):
     all_categories = Category.objects.all()
 
     return {"all_categories": all_categories}
+
+def product_info(request,slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {"product":product}
+
+    return render(request, "store/product-info.html", context)
